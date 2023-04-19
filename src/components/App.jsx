6639dcad7f9eddd86fac236231797dc5bd6de057
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-04-19 23:24:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Profile } from "./Profile";
+import user from '../user.json';
+
+import { Statistics } from "./Statistics";
+import data from '../data.json'
+
+import { FriendList } from "./FriendList";
+import friends from '../friends.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />
+      <FriendList items={friends} />
+    </>
   );
 };
