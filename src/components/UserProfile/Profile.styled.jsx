88e8e09791e95ca7da71d37a3e19c79a9a466336
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-04-23 21:35:32 +0300

    Create Profile.styled.jsx

---
+++ src/components/UserProfile/Profile.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ContainerProfile = styled.div`
+    width: 460px;
+    padding: 40px 60px;
+`;
+
+export const DescrProfile = styled.ul`
+     
+`;
+
+export const ListStats = styled.ul`
+    display: flex;
+    gap: 10px; 
+`;
+
